acb631e88b2e1b45b1d779e40f2583b204cde81e
Raksh-hac <54757042+Raksh-hac@users.noreply.github.com>
2024-11-26 22:00:36 +0530

    Update About.jsx

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import AnimatedTitle from '../ui/AnimatedLines';
 import { motion } from 'framer-motion';
 import ProfileImage from '../assets/Images/Profile.jpeg';
 import { TextGenerateEffect } from '../ui/TextGenrate';
-
-
-const buttonVariants = {
-  hover: {
-    scale: 1.1,
-    color: "white",
-    boxShadow: "0px 0px 8px rgb(255, 255, 255)",
-    backgroundColor: "black",
-    transition: {
-      yoyo: 10
-    }
-  }
-}
 
 const  AboutMe = () => {
   return (
@@ -29,45 +15,34 @@ const  AboutMe = () => {
         />
       </div>
 
-      <AnimatedTitle
-        title='about<br/>me'
-        containerClass="flex flex-col relative text-center text-3xl lg:text-8xl font-bold mb-4 z-50"/>
+      <h1 className='flex flex-col uppercase relative text-center justify-center text-3xl lg:text-6xl font-bold mb-4 z-50'>
+        More About <span className='flex justify-center'> Me </span>
+      </h1>
         
-        {/* <p className="relative z-10 text-white text-2xl font-bold px-24 justify-center leading-relaxed mb-6 text-center">
-        I'M AN INNOVATIVE DESIGNER AND DEVELOPER. 
-        MY PASSION FOR MINIMALIST AESTHETICS, ELEGANT DESIGN, AND INTUITIVE DEVELOPMENNT SHINES THROUGH IN MY WORK.
-        </p> */}
-
+        <div className='grid grid-cols-1 p-5 md:px-32'>
         <TextGenerateEffect
         words="I'M AN INNOVATIVE DESIGNER AND DEVELOPER. MY PASSION FOR MINIMALIST AESTHETICS, ELEGANT DESIGN, AND INTUITIVE DEVELOPMENNT SHINES THROUGH IN MY WORK."
-        className='relative z-10 text-white text-2xl font-bold px-24 justify-center leading-relaxed mb-6 text-center'
+        className='text-lg md:text-xl font-semibold text-justify md:text-center'
         filter={true}
         duration={0.4}
         />
 
-        <div className="relative z-10 text-white text-2xl px-24 justify-center leading-relaxed mb-6 text-center">
-          {/* <p>
-          I'm on the cutting edge of no-code tools that allow me to bring my creative visions to life. Though my methods may be unconventional, my dedication to the craft is unparalleled. I thrive on finding "unexpected solutions" and believe that with the right perspective, design can elevate the human experience.
-          </p> */}
-
           <TextGenerateEffect 
           words="I'm on the cutting edge of no-code tools that allow me to bring my creative visions to life. Though my methods may be unconventional, my dedication to the craft is unparalleled. I thrive on finding unexpected solutions and believe that with the right perspective, design can elevate the human experience."
+          className='text-lg md:text-xl font-semibold text-justify md:text-center'
           filter={true}
           duration={0.4}
           />
         </div>
 
-        <motion.button 
-          variants={buttonVariants}
-          whileHover="hover"
-        className="rounded-full uppercase font-bold text-xl px-4 py-2 border text-black border-white bg-slate-200">
+        <button className="hov-btn">
           <a 
-          href="./Resume.pdf" 
-          download="Resume.pdf" 
+          href="./resume.pdf" 
+          download="Vikash_Resume.pdf" 
           >
           download resume
-            </a>
-        </motion.button>
+          </a>
+        </button>
      
     </section>
   );
