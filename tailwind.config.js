96a1ac0f75d6f0082b89b687f2d7a9da516d4f1a
Raksh-hac <54757042+Raksh-hac@users.noreply.github.com>
2024-11-26 22:06:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      
+      animation: {
+        float: 'float 3s infinite linear',
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translateX(-50%)' },
+          '50%': { transform: 'translateX(50%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
     },
   },
   plugins: [],
