3ac12ecfe565adb03b022dd495891732b49db3b8
Raksh-hac <54757042+Raksh-hac@users.noreply.github.com>
2024-11-26 22:02:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 import { Link } from 'react-scroll'
-// import Dropdown from './Dropdown';
 
 function Navbar() {
-  // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  // const toggleDropdown = () => setIsDropdownOpen((prev) => !prev);
 
   return (
     <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-black text-white border-b-[0.5px] border-slate-500 z-50 overflow-x-hidden floating-nav">
@@ -18,7 +13,7 @@ function Navbar() {
         <Link
         to="home"
         smooth={true}
-        duration={500}
+        duration={350}
         className="nav-hover-btn cursor-pointer"
         >
           <a href="#home">Home</a>
@@ -26,7 +21,7 @@ function Navbar() {
         <Link
          to="skills"
          smooth={true}
-         duration={500}
+         duration={350}
          className="nav-hover-btn cursor-pointer"
         >
           <a href="#skills">Skills</a>
@@ -34,7 +29,7 @@ function Navbar() {
         <Link
          to="contact"
          smooth={true}
-         duration={500}
+         duration={350}
          className="nav-hover-btn cursor-pointer"
         >
           <a href="#contact">Contact</a>
@@ -42,35 +37,12 @@ function Navbar() {
         <Link
          to="about"
          smooth={true}
-         duration={500}
+         duration={350}
          className="nav-hover-btn cursor-pointer"
         >
           <a href="#contact">About</a>
         </Link>
       </div>
-
-      {/* Hamburger Menu (Visible on Small Screens) */}
-{/*       <div
-        className=" flex flex-col z-99 justify-center items-center space-y-1 cursor-pointer">
-        <span
-          className={`w-6 h-[2px] bg-white transition-transform duration-300 ${
-            isDropdownOpen ? 'rotate-45 translate-y-2' : ''
-          }`}
-        ></span>
-        <span
-          className={`w-6 h-[2px] bg-white transition-opacity duration-300 ${
-            isDropdownOpen ? 'opacity-0' : 'opacity-100'
-          }`}
-        ></span>
-        <span
-          className={`w-6 h-[2px] bg-white transition-transform duration-300 ${
-            isDropdownOpen ? '-rotate-45 -translate-y-2' : ''
-          }`}
-        ></span>
-      </div> */}
-
-      {/* Dropdown Component */}
-{/*       <AnimatePresence>{isDropdownOpen && <Dropdown onClose={toggleDropdown} />}</AnimatePresence> */}
     </nav>
   );
 }
